f52cea4b2ffb74db4189a252162f3ab6a10464b2
Jesse Shaw <jdshaw1987@gmail.com>
2018-08-18 20:11:43 -0400

    Updated KeyDown Functionalities

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import audio1 from './sounds/boom.wav'
+import clapAudio from './sounds/clap.wav'
+import hihhatAudio from './sounds/hihat.wav'
+import kickAudio from './sounds/kick.wav'
+import openhatAudio from './sounds/openhat.wav'
+import rideAudio from './sounds/ride.wav'
+import snareAudio from './sounds/snare.wav'
+import tomAudio from './sounds/tom.wav'
+import tinkAudio from './sounds/tink.wav'
 
 import Drumpad from './components/drumpad'
 
@@ -10,52 +19,122 @@ class App extends Component {
     this.state = { 
       currentSoundId: ''
     };
-    this.handleClickPlay = this.handleClickPlay.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-    this.playSound = this.playSound.bind(this);
-    this.Boom =  this.Boom.bind(this);
+    this.clapSelected = this.clapSelected.bind(this);
+    this.boomSelected = this.boomSelected.bind(this);
+    this.hiHatSelected = this.hiHatSelected.bind(this);
+    this.kickSelected = this.kickSelected.bind(this);
+    this.openHatSelected = this.openHatSelected.bind(this);
+    this.rideSelected = this.rideSelected.bind(this);
+    this.snareSelected = this.snareSelected.bind(this);
+    this.tomSelected = this.tomSelected.bind(this);
+    this.tinkSelected = this.tinkSelected.bind(this);
+
+    this.myfunction = this.myfunction.bind(this)
   }
 
-  handleClickPlay() {
-    console.log("Handle Click Play Triggered");
+  componentWillMount() {
+    document.addEventListener("keydown", this.onKeyPressed.bind(this));
   }
 
-  handleKeyPress() {
-    console.log("Handle Key Press Triggered");
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.onKeyPressed.bind(this));
+  }      
+
+  onKeyPressed(e) {
+    if(e.keyCode === 65) {
+      this.clapSelected()
+    }
+    if(e.keyCode === 83) {
+      this.hiHatSelected()
+    }
+    if(e.keyCode === 68) {
+      this.kickSelected()
+    }
+    if(e.keyCode === 70) {
+      this.openHatSelected()
+    }
+    if(e.keyCode === 71) {
+      this.boomSelected()
+    }
+    if(e.keyCode === 72) {
+      this.rideSelected()
+    }
+    if(e.keyCode === 74) {
+      this.snareSelected()
+    }
+    if(e.keyCode === 75) {
+      this.tomSelected()
+    }
+    if(e.keyCode === 76) {
+      this.tinkSelected()
+    }
   }
 
-  playSound() {
-    console.log("Play soundtriggered")
+  clapSelected() {
+    let audio = new Audio(`${clapAudio}`)
+    audio.play()
   }
 
-  Boom() {
-    console.log("Testing")
+  hiHatSelected() {
+    let audio = new Audio(`${hihhatAudio}`)
+    audio.play()
   }
+
+  kickSelected() {
+    let audio = new Audio(`${kickAudio}`)
+    audio.play()
+  }
+
+  openHatSelected() {
+    let audio = new Audio(`${openhatAudio}`)
+    audio.play()
+  }
+
+  boomSelected() {
+    let audio = new Audio(`${audio1}`)
+    audio.play()
+  }
+
+  rideSelected() {
+    let audio = new Audio(`${rideAudio}`)
+    audio.play()
+  }
+
+  snareSelected() {
+    let audio = new Audio(`${snareAudio}`)
+    audio.play()
+  }
+
+  tomSelected() {
+    let audio = new Audio(`${tomAudio}`)
+    audio.play()
+  }
+
+  tinkSelected() {
+    let audio = new Audio(`${tinkAudio}`)
+    audio.play()
+  }
+
+  myfunction() {
+    console.log("KEY DOWN")
+  }
+
 
   render() {
     return (
+
       <div className="App">
           <div className="keys">
-            <Drumpad inputKey="A" dataInputKey="65" sound="Clap"/>
-            <Drumpad inputKey="S" dataInputKey="83" sound="hihat"/>
-            <Drumpad inputKey="D" dataInputKey="68" sound="kick"/>
-            <Drumpad inputKey="F" dataInputKey="70" sound="openhat"/>
-            <Drumpad inputKey="G" dataInputKey="71" sound="boom" />
-            <Drumpad inputKey="H" dataInputKey="72" sound="ride" />
-            <Drumpad inputKey="J" dataInputKey="74" sound="snare" />
-            <Drumpad inputKey="K" dataInputKey="75" sound="tom" />
-            <Drumpad inputKey="L" dataInputKey="76" sound="tink" />
+            <Drumpad inputKey="A" dataInputKey="65" sound="Clap" onClick={this.clapSelected} onkeypress={this.myfunction}/>
+            <Drumpad inputKey="S" dataInputKey="83" sound="hihat" onClick={this.hiHatSelected}/>
+            <Drumpad inputKey="D" dataInputKey="68" sound="kick" onClick={this.kickSelected}/>
+            <Drumpad inputKey="F" dataInputKey="70" sound="openhat" onClick={this.openHatSelected}/>
+            <Drumpad inputKey="G" dataInputKey="71" sound="boom" onClick={this.boomSelected}/>
+            <Drumpad inputKey="H" dataInputKey="72" sound="ride" onClick={this.rideSelected}/>
+            <Drumpad inputKey="J" dataInputKey="74" sound="snare" onClick={this.snareSelected}/>
+            <Drumpad inputKey="K" dataInputKey="75" sound="tom" onClick={this.tomSelected} />
+            <Drumpad inputKey="L" dataInputKey="76" sound="tink" onClick={this.tinkSelected} />
           </div>
-
-          <audio data-key="65" src="sounds/clap.wav"></audio>
-          <audio data-key="83" src="sounds/hihat.wav"></audio>
-          <audio data-key="68" src="sounds/kick.wav"></audio>
-          <audio data-key="70" src="sounds/openhat.wav"></audio>
-          <audio data-key="71" src="sounds/boom.wav"></audio>
-          <audio data-key="72" src="sounds/ride.wav"></audio>
-          <audio data-key="74" src="sounds/snare.wav"></audio>
-          <audio data-key="75" src="sounds/tom.wav"></audio>
-          <audio data-key="76" src="sounds/tink.wav"></audio>
       </div>
     );
   }
--- src/components/drumpad.js
+++ src/components/drumpad.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Drumpad extends Component {
     render() {
         return(
-            <div data-key={this.props.dataInputKey} className="key" onClick={this.Boom}>
+            <div data-key={this.props.dataInputKey} className="key" onClick={this.props.onClick}>
                 <kbd>{this.props.inputKey}</kbd>
                 <span className="sound">{this.props.sound}</span>
             </div>
